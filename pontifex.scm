956bd735e6b4b78fddb7e6eb808da2de439307fb
John Anthony <johnanthony@lavabit.com>
2013-07-18 12:44:49 +0100

    Added still little ProgramMEN about

--- pontifex.scm
+++ pontifex.scm
@@ -1,3 +1,28 @@
+;; Copyright (C) 2013 John Anthony
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; ABOUT:
+;;
+;; The world's manliest encryption algorithm. The only encryption
+;; algorithm in which you can use the key to play poker.
+;; Based upon Bruce Schneier's (manly) work for Cryptonomicon.
+;; In Cryptonomicon the character Randy has a girlfriend who basically
+;; tells him to check his CIS privelege and stop metaphorically
+;; raping her with his (manly) beard. The rest of the book is Randy
+;; doing awesome stuff, thereby showing that she is stupid and wrong
+
 (use-modules (srfi srfi-1)
              (srfi srfi-26))
 
